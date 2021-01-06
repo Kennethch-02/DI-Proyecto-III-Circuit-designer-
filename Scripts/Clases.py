92ec642892eb976ca5953efc31db18ace9ebaf89
Kenneth C <kennethalonsoc@gmail.com>
2021-01-06 11:15:10 -0600

    Aplicacion basica que permite arrastrar objetos

--- Scripts/Clases.py
+++ Scripts/Clases.py
@@ -10,14 +10,44 @@ class Cursor(pygame.Rect):
         pygame.init()
         self.left, self.top = pygame.mouse.get_pos()
 
-class Line:
-    def __init__(self, anchura, x, y):
-        self.anchura = anchura
-        self.x = x
-        self.y = y
-    def set_anchura(self, anchura):
-        self.anchura = anchura
+class Cursor_sprite(pygame.sprite.Sprite):
+    def __init__(self, cursor):
+        pygame.init()
+        self.rect = cursor
+        self.rect.topleft = pygame.mouse.get_pos()
+
+class Line(pygame.Rect):
+    def __init__(self,x,y,w,h):
+        pygame.Rect.__init__(self, x,y,w,h)
+    def set_width(self, width):
+        self.width = width
+    def set_height(self, height):
+        self.height = height
     def set_x(self, x):
-        self.x = x
+        self.left = x
     def set_y(self, y):
-        self.y = y
+        self.top = y
+    def set_pos(self, pos):
+        self.left, self.top = pos
+
+class Line_(pygame.sprite.Sprite):
+    def __init__(self,surface, color , x,y,w,h):
+        pygame.sprite.Sprite.__init__(self)
+        self.line = Line(x,y,w,h)
+        self.color = color
+        self.surface = surface
+        self.rect = pygame.rect
+    def Draw(self):
+        self.rect = pygame.draw.rect(self.surface, self.color, self.line)
+    def set_width(self, width):
+        self.width = width
+    def set_height(self, height):
+        self.height = height
+    def set_x(self, x):
+        self.line.set_x(x)
+    def set_y(self, y):
+        self.line.set_y(y)
+    def set_pos(self,pos):
+        self.line.set_pos(pos)
+    def get_rect(self):
+        return self.line
--- Scripts/Main.py
+++ Scripts/Main.py
@@ -9,6 +9,7 @@ def Main():
     pantalla = Pantalla(WIDTH-2, HEIGHT-2)
     SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption('My Game')
+
     RED = (255, 0, 0)
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
@@ -16,18 +17,32 @@ def Main():
     GREEN = (0, 255, 0)
     BLUE = (0, 0, 255)
     YELLOW = (255, 255, 255)
+
+    line = Line_(SCREEN, BLACK, 10, 10, 100, 5)
+    Lista = pygame.sprite.Group()
+    Lista.add(line)
     is_running = True
+    is_down = False
+    SCREEN.fill(GREEN)
     while is_running:
         pygame.display.update()
         cursor.update()
+        SCREEN.fill(GREEN)
+        for e in Lista:
+            e.Draw()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
-        if(cursor.colliderect(pantalla)):
-            SCREEN.fill(GREEN)
-        else:
-            SCREEN.fill(RED)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                is_down = True
+                a = pygame.sprite.spritecollide(Cursor_sprite(cursor), Lista, False, False)
+                print(a)
+            if event.type == pygame.MOUSEBUTTONUP:
+                is_down = False
+        if is_down:
+            for sprite in a:
+                sprite.set_pos(pygame.mouse.get_pos())
     pygame.quit()
 
-t = Thread(target=Main())
+t = Thread(target=Main)
 t.start()
